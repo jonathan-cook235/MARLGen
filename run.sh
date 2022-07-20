d32d19811e7dd7c94ca14fe124e507909d8be0c7
Jonathan William Cook <jonathan.cook235@gmail.com>
2022-07-20 09:25:00 +0100

    add job submission

---
+++ run.sh
@@ -0,0 +1,51 @@
+d#$ -S /bin/bash
+#$ -N episodic-control-array
+#$ -cwd
+#$ -o $HOME/result-arr.out
+#$ -e $HOME/result-arr.err
+#$ -l h_rt=72:00:00
+#$ -l gpu=true
+#$ -R y
+#$ -l tmem=30G
+#$ -t 1-15
+
+
+echo "We're Starting!"
+hostname
+date
+
+export LANG="en_US.utf8"
+export LANGUAGE="en_US:en"
+
+
+PATH_TO_CONDA='/share/apps/miniconda3'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('${PATH_TO_CONDA}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "${PATH_TO_CONDA}/etc/profile.d/conda.sh" ]; then
+        . "${PATH_TO_CONDA}/etc/profile.d/conda.sh"
+    else
+        export PATH="${PATH_TO_CONDA}/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# Activate the correct gpu environment
+conda activate MARLGen
+
+# Get to the correct directory
+cd $HOME/
+
+# setup wandb
+wandb login 517ce6dd852cba92bb600d3bd3d3a05fca3aff13
+
+# run experiment with these arguments
+python epymarl/src/main.py 
+
+date
+echo "We're Done!"
