fb7101acb622553c441dc5c5a0f2f2a365c8553f
Jonathan William Cook <jonathan.cook235@gmail.com>
2022-08-11 10:14:27 +0100

    update logging

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #$ -S /bin/bash
 #$ -N episodic-control-array
 #$ -cwd
-#$ -o $HOME/result-arr-aug10-3.out
-#$ -e $HOME/result-arr-aug10-3.err
+#$ -o $HOME/result-arr-aug11-1.out
+#$ -e $HOME/result-arr-aug11-1.err
 #$ -l h_rt=12:00:00
 #$ -l gpu=true
 #$ -R y
