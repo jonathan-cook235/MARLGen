1d4d6f7b17ae214ebc663b7cca06672179a0b410
Jonathan William Cook <jonathan.cook235@gmail.com>
2022-08-14 11:47:57 +0100

    add env param

--- epymarl/src/controllers/basic_controller.py
+++ epymarl/src/controllers/basic_controller.py
@@ -8,11 +8,11 @@ class BasicMAC:
     def __init__(self, scheme, groups, args):
         self.n_agents = args.n_agents
         self.args = args
+        self.env = args.env
         input_shape = self._get_input_shape(scheme)
         self._build_agents(input_shape)
         self.agent_output_type = args.agent_output_type
         self.action_selector = action_REGISTRY[args.action_selector](args)
-
         self.hidden_states = None
 
     def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
@@ -65,7 +65,7 @@ class BasicMAC:
         self.agent.load_state_dict(th.load("{}/agent.th".format(path), map_location=lambda storage, loc: storage))
 
     def _build_agents(self, input_shape):
-        self.agent = agent_REGISTRY[self.args.agent](input_shape, self.args)
+        self.agent = agent_REGISTRY[self.args.agent](input_shape, self.args, self.env)
 
     def _build_inputs(self, batch, t):
         # Assumes homogenous agents with flat observations.
