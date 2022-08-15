b2ae45c4a1dd7b144a52b9bcf85f309aa6e103d2
Jonathan William Cook <jonathan.cook235@gmail.com>
2022-08-15 11:24:30 +0100

    herding exps

--- epymarl/src/run2.py
+++ epymarl/src/run2.py
@@ -185,10 +185,10 @@ def run_sequential(args, logger):
             avg_regret_tracker.append(avg_regret)
             avg_return_tracker.append(avg_return)
             wandb.log({'Avg Training Return (' + args.name + logging_env + str(
-                args.num_train_levels) + 'train)': avg_return})
+                args.num_train_levels) + 'train)': avg_return}, step=episode)
             if args.env == 'griddlygen':
                 wandb.log({'Avg Training Regret (' + args.name + logging_env + str(
-                    args.num_train_levels) + 'train)': avg_regret})
+                    args.num_train_levels) + 'train)': avg_regret}, step=episode)
             return_tracker = []
             regret_tracker = []
         buffer.insert_episode_batch(episode_batch)
@@ -219,14 +219,14 @@ def run_sequential(args, logger):
                     avg_val_return = np.mean(val_return_tracker)
                     if args.env == 'griddlygen':
                         wandb.log({'Generalisation Gap (' + args.name + logging_env + str(
-                            args.num_train_levels) + 'train)': avg_regret_tracker[-1] - avg_val_regret})
+                            args.num_train_levels) + 'train)': avg_regret_tracker[-1] - avg_val_regret}, step=episode)
                         wandb.log({'Avg Test Regret (' + args.name + logging_env + str(
-                            args.num_train_levels) + 'train)': avg_val_regret})
+                            args.num_train_levels) + 'train)': avg_val_regret}, step=episode)
                     else:
                         wandb.log({'Generalisation Gap (' + args.name + logging_env + str(
-                            args.num_train_levels) + 'train)': avg_return_tracker[-1] - avg_val_return})
+                            args.num_train_levels) + 'train)': avg_return_tracker[-1] - avg_val_return}, step=episode)
                     wandb.log({'Avg Test Return (' + args.name + logging_env + str(
-                        args.num_train_levels) + 'train)': avg_val_return})
+                        args.num_train_levels) + 'train)': avg_val_return}, step=episode)
                     val_regret_tracker = []
 
         if args.save_model and (
@@ -268,10 +268,10 @@ def run_sequential(args, logger):
                 avg_return = np.mean(return_tracker)
                 avg_regret = np.mean(regret_tracker)
                 wandb.log({'Avg Evaluation Return (' + args.name + logging_env + str(
-                    args.num_train_levels) +'train)': avg_return})
+                    args.num_train_levels) +'train)': avg_return}, step=episode-cur_episode)
                 if args.env == 'griddlygen':
                     wandb.log({'Avg Evaluation Regret (' + args.name + logging_env + str(
-                        args.num_train_levels) + 'train)': avg_regret})
+                        args.num_train_levels) + 'train)': avg_regret}, step=episode-cur_episode)
                 return_tracker = []
                 regret_tracker = []
 
---
+++ run_mappo_100_herding.sh
@@ -0,0 +1,63 @@
+#$ -S /bin/bash
+#$ -N episodic-control-array
+#$ -cwd
+#$ -o $HOME/result-arr-aug15-1.out
+#$ -e $HOME/result-arr-aug15-1.err
+#$ -l h_rt=12:00:00
+#$ -l gpu=true
+#$ -R y
+#$ -l tmem=32G
+#$ -t 1-5
+
+
+echo "We're Starting!"
+hostname
+date
+
+export LANG="en_US.utf8"
+export LANGUAGE="en_US:en"
+
+
+PATH_TO_CONDA='/share/apps/miniconda3'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('${PATH_TO_CONDA}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "${PATH_TO_CONDA}/etc/profile.d/conda.sh" ]; then
+        . "${PATH_TO_CONDA}/etc/profile.d/conda.sh"
+    else
+        export PATH="${PATH_TO_CONDA}/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# Activate the correct gpu environment
+conda activate MARLGen
+
+# Get to the correct directory
+cd $HOME/
+
+# setup wandb
+wandb login 8f0ba26a350a67397b8e8abdc7865b1feb4a0b46
+
+# set vulkan environment variables
+# export VULKAN_SDK='/vulkan/1.3.216.0/x86_64'
+# export PATH="${VULKAN_SDK}/bin:$PATH"
+# export LD_LIBRARY_PATH="${VULKAN_SDK}/lib:$LD_LIBRARY_PATH"
+# export VK_LAYER_PATH="${VULKAN_SDK}/etc/explicit_layer.d"
+
+export VULKAN_SDK=~/vulkan/1.3.216.0/x86_64
+export PATH=$VULKAN_SDK/bin:$PATH
+export LD_LIBRARY_PATH=$VULKAN_SDK/lib:$LD_LIBRARY_PATH
+export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d
+
+echo "Running experiment..."
+# run experiment with these arguments
+python Dissertation/epymarl/src/main_mappo_100_herding.py 
+
+date
+echo "We're Done!"
